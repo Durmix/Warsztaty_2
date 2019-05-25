62b99c962e80d50f7ac810d0494ea0546418e7bf
Durmix <kacperdurmaj@gmail.com>
2019-05-25 15:49:15 +0200

    Working

---
+++ src/src/Main.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        /*User user1 = new User("Kamil", "kamil@wp.pl", "qwerty");
+        DbService.getInstance().setDb("Warsztaty_2", "root", "coderslab");
+        user1.saveToDB(DbService.getInstance().createConn());
+        User user2 = User.loadUserById(DbService.getInstance().createConn(),3);
+        user2.setEmail("aleksander@gmail.com");
+        user2.setPassword("zxcvbn");
+        user2.setUsername("Aleksander");
+        user2.saveToDB(DbService.getInstance().createConn());
+        User user3 = User.loadUserById(DbService.getInstance().createConn(), 4);
+        user3.delete(DbService.getInstance().createConn());
+        User[] users = User.loadAllUsers(DbService.getInstance().createConn());
+        for(int i = 0; i < users.length; i++) {
+            System.out.print(users[i].getId() + " ");
+            System.out.print(users[i].getUsername() + " ");
+            System.out.println(users[i].getEmail());
+        }*/
+
+        
+    }
+
+}
